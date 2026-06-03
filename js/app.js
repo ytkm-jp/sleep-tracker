e2669f7c741bb37fb2fe9e06059a2e1cb4b95936
ytkm-jp <ytkm-jp@users.noreply.github.com>
2026-06-03 23:46:46 +0900

    セキュリティ強化（API制限）

--- js/app.js
+++ js/app.js
@@ -1,16 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, collectionGroup, query } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAuth, signInWithPopup, signInWithRedirect, getRedirectResult, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCw_JbTLEW4vfGDkF624dpCf5MMtyTYB7E",
-    authDomain: "sleep-tracker-b2ffd.firebaseapp.com",
-    projectId: "sleep-tracker-b2ffd",
-    storageBucket: "sleep-tracker-b2ffd.firebasestorage.app",
-    messagingSenderId: "904987298822",
-    appId: "1:904987298822:web:ccd101593cf09032714787",
-    measurementId: "G-BT13C46MWT"
-};
+import { firebaseConfig } from "./firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
---
+++ js/firebase-config.example.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "YOUR_FIREBASE_WEB_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.firebasestorage.app",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_FIREBASE_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+};
