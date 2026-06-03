6eba831738bd2281ac9cfef72e41abf487299536
ytkm-jp <ytkm-jp@users.noreply.github.com>
2026-06-04 00:53:17 +0900

    gitの公開範囲変更

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCw_JbTLEW4vfGDkF624dpCf5MMtyTYB7E",
+    authDomain: "sleep-tracker-b2ffd.firebaseapp.com",
+    projectId: "sleep-tracker-b2ffd",
+    storageBucket: "sleep-tracker-b2ffd.firebasestorage.app",
+    messagingSenderId: "904987298822",
+    appId: "1:904987298822:web:ccd101593cf09032714787",
+    measurementId: "G-BT13C46MWT"
+};
